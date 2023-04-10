e4ffc59de9a187caad07e22d3f504f74955a48d8
@AdaezeEgba <helenadaeze@gmail.com>
2023-04-10 01:52:58 +0100

    class code commit

---
+++ testing.js
@@ -0,0 +1,22 @@
+const buttons= document.getElementsByClassName("button");
+function buttonRed(){
+    
+    buttons[0].style.backgroundColor='red';
+   
+};
+
+function buttonYellow(){
+   
+   buttons[1].style.backgroundColor='yellow';
+ 
+};
+function buttonBlue(){
+  
+   buttons[2].style.backgroundColor='blue';
+    
+};
+function buttonGreen(){
+
+buttons[3].style.backgroundColor=	'green';
+
+};
